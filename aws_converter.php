ce351f967bfc5caa720cc798f59acb2fb4ccc302
Ricardo <devmaker.ricardo@gmail.com>
2017-05-24 16:22:51 -0300

    add sS sub-node

--- aws_converter.php
+++ aws_converter.php
@@ -3,18 +3,14 @@
  * *nix pipe handler
  */
 $input_stream = fopen("php://stdin","r");
-header('Content-Type: application/json');
-
-$json = array();
 while($line = fgets($input_stream))
 {
 	$array = json_decode($line,true);
 	$array = aws_converter($array);
-	array_push($json, $array);
+	print(json_encode($array)."\n");
 }
-
-print(json_encode($array)."\n");
 fclose($input_stream);
+
 
 /*
  * conversion function from string attributes to proper JSON.
@@ -24,8 +20,9 @@ function aws_converter($old) {
 	foreach($old as $k=>$v) {
 		if(!empty($v['s']) && substr($v['s'], 0, 1) == "{") { 
 			$new[$k] = json_decode($v['s'], true);
+		} else {
+			$new[$k] = isset($v['s']) && !is_null($v['s']) ? $v['s'] : $v['sS'];
 		}
-		else { $new[$k] = $v['s']; }
 	}
 	return $new;
 }
